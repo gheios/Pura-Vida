cb5e9f05b4a120b5d641c758cd306dda89829c0a
Brayan David Agudelo <gheios000@gmail.com>
2022-08-29 13:16:50 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let app = document.getElementById('typewriter');
+ 
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+ 
+typewriter
+  .pauseFor(2500)
+  .typeString('Proxima carrera, Inscribite')
+  .pauseFor(200)
+  .deleteChars(10)
+  .start();
+
+
+
+  /*table*/
